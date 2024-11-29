7a42c84620db015271e7c729b80c1d86b57eb8e7
XamitlHernandez <xamitlhernandez@gmail.com>
2024-11-29 15:02:58 -0600

    codigo en pares

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
 // Escribe tu código aquí.
+let a ="mosca";
+const b= ["arboles", "robles","nom"];
+let d=[];
+function devolverValores(a,b){
+for (let i=0  ; i<b.length;i++){
+    let c = b[i];
+    if (a.length<c.length){
+        d.push(c);
+    }
+}
+return d;
+}
+function crearLista(d){
+    for(let j=0; j<d.length;j++){
+     document.querySelector("ul").innerHTML= `<li> ${d[j]} </li>`;
+    }
+}
+devolverValores(a,b);
+crearLista(d);
